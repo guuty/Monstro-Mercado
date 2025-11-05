b36530e39e66703f579dc142ddf95944434ba693
Gutty <gustavoleonelferreyra@gmail.com>
2025-11-05 17:21:13 -0300

    Corrijo error 500 y configuración de variables

--- mymarket/settings.py
+++ mymarket/settings.py
@@ -35,7 +35,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME]
 else:
-    ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+    ALLOWED_HOSTS = ["127.0.0.1", "localhost", '.onrender.com']
 
 # Application definition
 
